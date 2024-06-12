d532e18a767e480a01890e6645af596a93beb89d
Allana Yzabelle Diaz <allanayzabelle.diaz@wvsu.edu.ph>
2024-06-12 23:08:07 +0800

    intial website design

---
+++ src/app/components/SplashScreen.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import React, { useEffect } from 'react';
+import anime from 'animejs';
+import Image from 'next/image';
+
+interface SplashScreenProps {
+  finishLoading: () => void;
+}
+
+const SplashScreen: React.FC<SplashScreenProps> = ({ finishLoading }) => {
+  useEffect(() => {
+    const loader = anime.timeline({
+      complete: () => finishLoading()
+    });
+    loader.add({
+      targets: "#logo",
+      delay: 0,
+      scale: 3,
+      duration: 2000,
+      easing: "easeInOutExpo",
+    });
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center overflow-clip">
+          <Image
+            src="/hug.gif"
+            alt="Hug"
+            className="unoptimized"
+            width={1100}
+            height={1100}
+            ></Image>
+   </main>
+  );
+};
+
+export default SplashScreen;
